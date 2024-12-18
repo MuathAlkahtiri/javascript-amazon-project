aded03f989a22417238e2639b872e9c18607b162
MuathAlkathiri <Mo3a4_11@hotmail.com>
2024-12-18 19:50:20 +0000

    deleting product from cart

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,6 @@
-export const cart=[{
+export let cart=[{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2,
+  quantity: 2
 },
 {
   productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
@@ -23,5 +23,15 @@ export function addToCart(productId){
           
         });
       }
+
+  }
+ export function removeFromCart(productId){
+    let newCart =[];
+    cart.forEach((cartItem)=>{
+      if(cartItem.productId !== productId){
+        newCart.push(cartItem);
+      }
+      cart=newCart;
+    })
 
   }
