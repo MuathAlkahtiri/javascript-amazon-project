2ea5dccf9be2caba7aaa260c43ffb88b3aa6042f
MuathAlkathiri <Mo3a4_11@hotmail.com>
2024-12-18 20:07:20 +0000

    delete products from HTML when click delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -12,7 +12,8 @@ if(product.id === productId){
 }
  });
 cartSummaryHTML +=`
-<div class="cart-item-container">
+<div class="cart-item-container
+ js-cart-item-container-${matchingItem.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -98,6 +99,9 @@ document.querySelectorAll('.js-delete-link')
         link.addEventListener('click' , () => {
         const productId =link.dataset.productId;
         removeFromCart(productId);
-        console.log(cart);
+        const container = document.querySelector(
+            `.js-cart-item-container-${productId}`
+          );
+          container.remove();
         });
     });
